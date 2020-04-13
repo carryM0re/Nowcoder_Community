9f1189b3c4645c2eb7b7229bc903a9dc38e5e4ae
carryM0re <635705616@qq.com>
2020-04-13 17:12:30 +0800

    通过ES完成搜索功能

--- src/main/java/com/monsoon/community/CommunityApplication.java
+++ src/main/java/com/monsoon/community/CommunityApplication.java
@@ -4,8 +4,17 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class CommunityApplication {
+
+	@PostConstruct
+	public void init(){
+		// 解决netty启动冲突问题
+		// Netty4Utils里找到的问题
+		System.setProperty("es.set.netty.runtime.available.processors", "false");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommunityApplication.class, args);
